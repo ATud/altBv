4888c1fb88815588875a1a9c411b703921ff6996
atudor <adtudor@pentalog.fr>
2014-11-21 23:15:59 +0200

    Revert "alt-bv-step2"
    This reverts commit ef5fadbeaf5a2dd3500fea0b9bfc44c342a4b555.

--- main.js
+++ main.js
@@ -5,30 +5,3 @@ MyApp.addRegions({
   tableRegion: "#content",
   detailRegion: "#details"
 });
-
-
-// ---- Backbone Event Agregator ----- 
-MyApp.vent.on('change:api', function(newId){
-	MyApp.detailRegion.currentView.collection.changeUrl(newId);
-})
-
-MyApp.addInitializer(function(options){
-	var teamsView = new TeamsView({
-		collection: options.teams
-	});
-
-	var detailsView = new AllDetailsView({
-		collection: new DetailsCollection({
-			id : 'mralexgray'
-		})
-	});
-
-	MyApp.tableRegion.show(teamsView);
-	MyApp.detailRegion.show(detailsView);
-});
-
-
-$(document).ready(function(){
-    var teams = new TeamsCollection(staticData);
-    MyApp.start({teams: teams});
-});
